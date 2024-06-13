dc3146b726f5f2a8a27aefe953d8a7b2797da7fa
Petr3336 <petr.petr3336@gmail.com>
2024-06-13 15:24:16 +0300

    Remove infinity token update request when resfresh token invalid

--- oop_project.client/src/axios/AxiosConfiguration.js
+++ oop_project.client/src/axios/AxiosConfiguration.js
@@ -17,23 +17,38 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
+let retryCount = 0; // Счетчик попыток
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response.status === 401) {
-      // Обновляем токен
-      return axiosInstance
-        .post("/api/refresh", {
-          refreshToken: localStorage.getItem("refreshToken"),
-        })
-        .then((response) => {
-          const newToken = response.data.accessToken; // Замените на ваш метод получения нового токена
-          const newRefresh = response.data.refreshToken;
-          localStorage.setItem("accessToken", newToken);
-          localStorage.setItem("refreshToken", newRefresh);
-          console.log(error.config)
-          return axiosInstance.request(error.config); // Перезапускаем запрос с новым токеном
-        });
+      if (localStorage.getItem("refreshToken") && retryCount < 3) {
+        retryCount++;
+        // Обновляем токен
+        return axiosInstance
+          .post("/api/refresh", {
+            refreshToken: localStorage.getItem("refreshToken"),
+          })
+          .then((response) => {
+            retryCount = 0;
+            const newToken = response.data.accessToken; // Замените на ваш метод получения нового токена
+            const newRefresh = response.data.refreshToken;
+            localStorage.setItem("accessToken", newToken);
+            localStorage.setItem("refreshToken", newRefresh);
+            console.log(error.config);
+            return axiosInstance.request(error.config); // Перезапускаем запрос с новым токеном
+          })
+          .catch((error) => {
+            console.log("err");
+            localStorage.removeItem("refreshToken");
+            localStorage.removeItem("accessToken");
+            throw error;
+          });
+      } else {
+        localStorage.removeItem("refreshToken");
+        localStorage.removeItem("accessToken");
+      }
     }
     return Promise.reject(error);
   }
